fcb93a1e5204e8f8ad9153ecbc0985e4af5724b2
Murmanox <murmanox@hotmail.com>
2019-12-16 00:17:08 +0000

    Started building a comic browser to display comics saved to your computer.

--- comic_browser.py
+++ comic_browser.py
@@ -1,133 +1,211 @@
+import subprocess
 import sys
 import os
 import PySide2
 from zipfile import ZipFile
 import config
+from flow_layout import FlowLayout
+from pathlib import Path
 
-from PySide2 import QtWidgets, QtGui
-from PySide2.QtCore import QSize
+from PySide2 import QtWidgets, QtGui, QtCore
+from PySide2.QtCore import QSize, QPoint
 from PySide2.QtWidgets import QWidget, QPushButton, QToolTip, QApplication, QMessageBox, QMainWindow, QAction, QMenu, \
-    QLabel, QSizePolicy, QGridLayout, QHBoxLayout
+    QLabel, QSizePolicy, QGridLayout, QHBoxLayout, QVBoxLayout, QSpacerItem, QScrollArea
 from PySide2.QtGui import QIcon, QFont, QKeySequence, Qt, QPixmap, QPalette, QColor, QImage
 
-TEST_DIRECTORY = r"D:\Porn\Hentai\test"
 settings = config.Settings()
+
+ZIP_TYPES = settings.zip_types
+IMAGE_TYPES = settings.image_types
+DIRECTORIES = settings.directories
+
+# TODO: Use zoom value to change size of comics in explorer
+# TODO: Allow filter panel to be resized with mouse
 
 
 class Comic(QLabel):
-    image_types = ["jpg", "png"]
-    zip_types = ["cbz", "zip"]
+    scaleX = 145
+    scaleY = 200
 
-    def __init__(self, path=None, parent=None):
+    spaceX = 5
+
+    def __init__(self, path: str, image: QPixmap, parent=None):
         super(Comic, self).__init__(parent)
+        self.path = path
+        self.image = image
+        self.thumbnail = None
 
-        self.path = path.lower()
-        self.parent = parent
-        self.filename, self.file_type = self.path.split(".")
+        # set_colour(self, Qt.yellow)
+        self.set_image()
 
-        image_file = (self.file_type in self.image_types)
-        zip_file = (self.file_type in self.zip_types)
+    def set_image(self):
+        landscape = (self.image.size().width() > self.image.size().height())
 
-        print(f"testing {self.path}")
-        if zip_file:
-            self.image = self._image_from_zip(path)
-        elif image_file:
-            pass
-        else:
-            raise ValueError(self.file_type)
+        if landscape:
+            self.scaleX = (self.scaleX * 2) + self.spaceX
 
-        print(f"Displaying {self.path}")
-
-        self.thumbnail = self.image.scaled(parent.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
-        self.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
-        self.setPixmap(self.image)
-
-        print("finished")
-
-    def _image_from_zip(self, path):
-        with ZipFile(path, "r") as my_zip:
-            for x in my_zip.filelist:
-                if x.filename.split(".")[-1] in self.image_types:
-                    image = my_zip.read(x)
-                    qp = QPixmap()
-                    qp.loadFromData(image)
-                    image = qp
-                    print("Image found")
-                    break
-        return image if image else QPixmap()
-
-    def resizeEvent(self, event: PySide2.QtGui.QResizeEvent):
-        self.thumbnail = self.image.scaled(self.parent.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        self.thumbnail = self.image.scaled(self.scaleX, self.scaleY, Qt.IgnoreAspectRatio, Qt.SmoothTransformation)
         self.setPixmap(self.thumbnail)
+        self.setFixedSize(self.scaleX, self.scaleY)
+        self.setAlignment(Qt.AlignCenter)
+
+    def contextMenuEvent(self, ev: PySide2.QtGui.QContextMenuEvent) -> None:
+        """
+        Handles the context menu that appears when you right click on a comic and their actions.
+
+        :param ev: Event parameter used by PySide2, stores all the information about the right click event.
+        """
+        cmenu = QMenu()
+        open_menu = QMenu("Open")
+
+        open_menu.addAction("Open in HentaiReader")
+        open_menu.addAction("Open in New Tab")
+        open_menu.addAction("Open in Explorer", self.open_in_explorer)
+
+        cmenu.addMenu(open_menu)
+        cmenu.addAction("Info", lambda: print(self.path))
+        cmenu.exec_(self.mapToGlobal(ev.pos()))
+
+    def open_in_explorer(self):
+        """
+        Opens the targeted comic's location in file explorer / finder.
+        :return:
+        """
+        if sys.platform == "win32":
+            subprocess.Popen(f"explorer /select, {self.path}")
+        else:
+            print("OS not supported yet")
 
 
-class ComicWidget(QWidget):
+class ComicExplorer(QWidget):
     def __init__(self, parent, colour: QColor = None):
-        super(ComicWidget, self).__init__(parent)
+        super(ComicExplorer, self).__init__(parent)
 
         self.colour = colour
-        self.layout = QGridLayout()
-        self.displayed_comics = {}
+        self.layout = FlowLayout(self)
 
         if self.colour:
-            self.colour_widget()
+            set_colour(self, self.colour)
 
         self.init_ui()
 
-    def colour_widget(self):
-        bg = QPalette()
-        bg.setColor(QPalette.Window, self.colour)
-
-        self.setPalette(bg)
-        self.setAutoFillBackground(True)
-
     def init_ui(self):
-        self.setLayout(self.layout)
+        set_colour(self, self.colour)
+        self.display_comics()
 
-        zip_types = ["cbz", "zip"]
-        count = 0
-        for directory in settings.directories:
+    def display_comics(self):
+
+        def rename(directory):
+            print(f"Searching {directory}")
             for file in os.listdir(directory):
-                comic_path = os.path.join(TEST_DIRECTORY, file)
-                file_type = file.split(".")[-1]
+                file_type = suffix(file)
+                comic_path = os.path.join(directory, file)
 
-                if file_type in zip_types:
-                   comic = Comic(comic_path, self)
+                # If file is folder then search folder
+                if file_type == "":
+                    rename(comic_path)
+                    continue
 
-                if comic:
-                    self.layout.addWidget(comic, 0, count)
-                    count += 1
+                # If file is a supported file type and contains an image then create Comic object.
+                if file_type in ZIP_TYPES:
+                    image = _image_from_zip(comic_path)
+
+                    if image:
+                        self.layout.addWidget(Comic(comic_path, image, self))
+
+        for x in DIRECTORIES:
+            rename(x)
+
+
+def _image_from_zip(path: str) -> QPixmap:
+    """
+    Open an archive file at path and return a QPixmap containing the first image found in the archive.
+    If no supported images found, returns None.
+
+    :param path: Path to the comic book archive including file name and extension.
+    :return: QPixmap
+    """
+
+    with ZipFile(path, "r") as my_zip:
+        for x in my_zip.filelist:
+            if suffix(x.filename) in IMAGE_TYPES:
+                image = my_zip.read(x)
+                qp = QPixmap()
+                qp.loadFromData(image)
+                image = qp
+                return image
+
+
+def suffix(path):
+    return Path(path).suffix
 
 
 class MainWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self, debug=False):
         super(MainWindow, self).__init__()
 
-        self.layout = QGridLayout()
-        self.central_widget = QWidget(self)
-        self.setCentralWidget(self.central_widget)
-        self.centralWidget().setLayout(self.layout)
-
-        bg = QPalette()
-        bg.setColor(QPalette.Window, Qt.red)
-        self.central_widget.setPalette(bg)
-        self.central_widget.setAutoFillBackground(True)
-
-        self.comic_widget = ComicWidget(self, Qt.green)
-        self.layout.addWidget(self.comic_widget)
+        self.debug = debug
+        self.layout = QHBoxLayout()
 
         self.init_ui()
 
     def init_ui(self):
-        self.resize(500, 300)
+        self.resize(1000, 900)
         self.setWindowTitle("HentaiReader")
         icon = QIcon(QPixmap("images/count.png"))
         self.setWindowIcon(icon)
 
+        self.setCentralWidget(make_widget(self, Qt.red, debug=self.debug))
+        self.centralWidget().setLayout(self.layout)
+
+        filter_panel = make_widget(self, Qt.blue, debug=self.debug)
+        filter_panel.setFixedWidth(250)
+        self.layout.addWidget(filter_panel)
+
+        comic_explorer = ComicExplorer(self, Qt.green)
+        self.layout.setMargin(3)
+        self.layout.setSpacing(5)
+
+        scroll_area = QScrollArea(self)
+        scroll_area.setWidget(comic_explorer)
+        scroll_area.setWidgetResizable(True)
+        self.layout.addWidget(scroll_area)
+
+
+def set_colour(obj, colour):
+    bg = QPalette()
+    bg.setColor(QPalette.Window, colour)
+    obj.setPalette(bg)
+    obj.setAutoFillBackground(True)
+
+
+def make_widget(parent, colour: QColor = None, *, max_height: int = None, debug: bool = None) -> QWidget:
+    widget = QWidget(parent)
+
+    if max_height is not None:
+        widget.setMaximumHeight(max_height)
+
+    if colour and debug:
+        set_colour(widget, colour)
+    return widget
+
+
+def make_layout(parent=None, layout=None, *, margin: int = None, spacing: int = None):
+    layout = layout()
+
+    if margin is not None:
+        layout.setMargin(margin)
+
+    if spacing is not None:
+        layout.setSpacing(spacing)
+
+    parent.setLayout(layout)
+    return layout
+
 
 def main():
     app = QtWidgets.QApplication([])
-    root = MainWindow()
+    root = MainWindow(debug=True)
     root.show()
     sys.exit(app.exec_())
 
--- config.py
+++ config.py
@@ -1,7 +1,21 @@
 class Settings:
+    """
+    This is where application-wide settings are stored. Currently they are hard coded, but in the future
+    they will be read from a file at launch to maintain settings over multiple application launches.
+    """
     def __init__(self):
-        self.anti_alias = True
+        # FILE SETTINGS
+        self.zip_types = [".cbz", ".zip"]
+        self.image_types = [".jpg", ".jpeg", ".png"]
         self.directories = [r"D:\Porn\Hentai\test"]
 
+        # IMAGE SETTINGS
+        self.anti_alias = True
 
-config = Settings()
+        # WINDOW SETTINGS
+
+        # COMIC EXPLORER SETTINGS
+        self.zoom = 1
+
+        # DEBUG SETTINGS
+        self.debug = True
