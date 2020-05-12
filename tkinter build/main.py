fd044ab485ecdc1e9c986fa40b77e8570b8f2655
adam-drake1 <65261008+adam-drake1@users.noreply.github.com>
2020-05-13 00:58:31 +0100

    Update main.py

--- tkinter build/main.py
+++ tkinter build/main.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
 
     # comic = r"C:\Users\adrake.KEMPTOWN\Pictures\onepunchman\onepunchman.cbz"
     # comic = r"D:\Downloads\gallery.zip"
-    comic = "D:\Porn\Hentai\Doujinshi\Tamarun\Andria La Land\Andira La La Land II (2018).cbz"
 
     my_gui = ImageViewer(comic)
     show_vars(config)
